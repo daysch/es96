898b96557cf3b401d5f05d6fe779b55843510f25
Amy Brooke <amybrooke@users.noreply.github.com>
2020-03-21 13:04:48 +0000

    Update barcode2Weight.py

--- barcode2Weight.py
+++ barcode2Weight.py
@@ -2,7 +2,7 @@ def barcode2weight(barcode):
     
     """
     Takes in one int, the barcode.
-    Queries the database warehouse for the weight and unit associated with the barcode and returns these values.
+    Queries the database es96 for the weight and unit associated with the barcode and returns these values.
     """
     
     import mysql.connector
