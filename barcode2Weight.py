edf661fb06422772f5316a55e96f8bfd732cd20c
Davey Schoenberg <dschoenberg@Anonyputer.local>
2020-03-22 11:21:13 -0400

    simplify barcode2Weight

--- barcode2Weight.py
+++ barcode2Weight.py
@@ -1,27 +1,19 @@
 def barcode2Weight(barcode):
-    
+
     """
     Takes in one int, the barcode.
     Queries the database es96 for the weight and unit associated with the barcode and returns these values.
     """
-    
-    import mysql.connector
 
-    # Establish connection to the database
-    conn = mysql.connector.connect(
-          host="localhost",
-          user="root",
-          password="ES96database",
-          database="es96")
-
-    # Create a cursor
-    cursor = conn.cursor()
+    import sqlite3
+    conn = sqlite3.connect('es96.db')
+    c = conn.cursor()
 
     # Query the database for the weight and unit associated with the scanned barcode
-    cursor.execute("SELECT weight FROM products WHERE barcode = {}".format(barcode))
+    c.execute("SELECT weight FROM products WHERE barcode = {}".format(barcode))
     weight = cursor.fetchall()[0][0]
 
-    cursor.execute("SELECT unit FROM products WHERE barcode = {}".format(barcode))
+    c.execute("SELECT unit FROM products WHERE barcode = {}".format(barcode))
     unit = cursor.fetchall()[0][0]
-    
+
     return weight, unit
