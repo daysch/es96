4439c54f75e79263988a4a78652eaf4a991f4b7f
Amy Brooke <amybrooke@users.noreply.github.com>
2020-03-23 12:39:57 +0000

    Updates

--- barcode2OrderQuantity.py
+++ barcode2OrderQuantity.py
@@ -1,24 +1,17 @@
 def barcode2OrderQuantity(barcode):
-    
     """
     Takes in one int, the barcode.
     Queries the database es96 for the order quantity associated with the order and barcode and returns these values.
     """
-    
-    import mysql.connector
 
-    # Establish connection to the database
-    conn = mysql.connector.connect(
-          host="localhost",
-          user="root",
-          password="ES96database",
-          database="es96")
+    import sqlite3
 
-    # Create a cursor
-    cursor = conn.cursor()
+    # Make a connection to the database
+    conn = sqlite3.connect('es96.db')
+    c = conn.cursor()
 
     # Query the database for the weight and unit associated with the scanned barcode
-    cursor.execute("SELECT quantity FROM orders WHERE barcode = {}".format(barcode))
-    quantity = cursor.fetchall()[0][0]
+    c.execute("SELECT quantity FROM orders WHERE barcode = {}".format(barcode))
+    quantity = c.fetchall()[0][0]
 
     return quantity
