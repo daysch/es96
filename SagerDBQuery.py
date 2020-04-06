9a736aa62b7c9c8081c2e05ae38dac6bbab2a20f
Amy Brooke <amybrooke@users.noreply.github.com>
2020-04-06 09:34:22 +0100

    initial work on JDBC connection

--- SagerDBQuery.py
+++ SagerDBQuery.py
@@ -1,1 +1,11 @@
+# query is a string of SQL commands to query the Sager Database for the product order details: ID, quantity, weight
+# curs is a cursor object from calling SetupConn
+def SagerDBQuery(curs, query):
 
+    curs.execute(query)
+    order = curs.fetchall()
+
+    # curs.close()
+    # conn.close()
+
+    return order
--- SetupConn.py
+++ SetupConn.py
@@ -1,1 +1,7 @@
+import jaydebeapi
 
+# Takes no inputs and returns a cursor object once setting up a JDBC connection with the Oracle Database
+def SetupConn():
+    conn = jaydebeapi.connect('oracle.jdbc.driver.OracleDriver','[MYUSER]/[MYPASS]@(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=[MYHOST])(PORT=1521))(CONNECT_DATA=(SERVER=dedicated) (SERVICE_NAME=[MYSERVICENAME])))')
+    curs = conn.cursor()
+    return curs
