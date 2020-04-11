f7ea0e243cded478f5af5ed61efa02e443c60ddb
MatthiasFi <31547152+MatthiasFi@users.noreply.github.com>
2020-04-11 09:12:56 +0200

    Update Sager_counting.bat

--- Sager_counting.bat
+++ Sager_counting.bat
@@ -1,7 +1,7 @@
 @echo off
 REM Replace the path with whatever the path to you executable is
 set url="http://127.0.0.1:5000"
-start chrome "%url%"
+start microsoft-edge:"%url%"
 
 REM Replace the path with whatever the path to you executable is
 python "C:\Users\Matthias Fischer\Documents\GitHub\es96-master\es96-master\application.py %*"
