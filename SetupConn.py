e45e9d02b9fdcbafb6ac55dc25557d149894e552
Amy Brooke <amybrooke@college.harvard.edu>
2020-04-17 12:16:41 +0100

    setup connection to sager's db

--- SetupConn.py
+++ SetupConn.py
@@ -1,7 +1,7 @@
 import jaydebeapi
+import jpype
 
-# Takes no inputs and returns a cursor object once setting up a JDBC connection with the Oracle Database
-def SetupConn():
-    conn = jaydebeapi.connect('oracle.jdbc.driver.OracleDriver','[MYUSER]/[MYPASS]@(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=[MYHOST])(PORT=1521))(CONNECT_DATA=(SERVER=dedicated) (SERVICE_NAME=[MYSERVICENAME])))')
-    curs = conn.cursor()
-    return curs
+jvm_path = jpype.getDefaultJVMPath()
+jpype.startJVM(jvm_path, '-Djava.class.path=C:\ojdbc10.jar')
+con = jaydebeapi.connect("oracle.jdbc.driver.OracleDriver", "jdbc:oracle:thin:@wmsdbtst01.sager.com:1521:MV10TST",["TSTMOVE","TSTMOVE"])
+curs = con.cursor()
