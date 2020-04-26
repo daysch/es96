eaf392a3aaa95456dd9af5cf4bc6a3d2d2aec1bd
MatthiasFi <mfischer@users.noreply.github.com>
2020-04-26 18:54:20 +0200

    cursor working baby

--- Sager_counting_setup.bat
+++ Sager_counting_setup.bat
@@ -1,15 +1,21 @@
-echo y | pip uninstall flask 
-echo y | pip uninstall pyusb 
-echo y | pip uninstall usb 
-echo y | pip uninstall werkzeug==0.16.0 
-echo y | pip uninstall flask_session 
-echo y | pip uninstall Flask_wtf 
+REM first try to uninstall, then reinstall all packages
+echo y | pip uninstall flask
+echo y | pip uninstall pandas
+echo y | pip uninstall pyusb
+echo y | pip uninstall usb
+echo y | pip uninstall werkzeug==0.16.0
+echo y | pip uninstall flask_session
+echo y | pip uninstall Flask_wtf
+echo y | pip uninstall jaydebeapi
+echo y | pip uninstall Jpype1==0.6.3
 
 pip install flask
 pip install pyusb
+pip install pandas
 pip install usb
 pip install werkzeug==0.16.0
 pip install flask_session
 pip install Flask_wtf
-
-pause
+pip install jaydebeapi
+pip install jaydebeapi
+pip install Jpype1==0.6.3
