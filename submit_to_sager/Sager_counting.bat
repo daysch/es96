c77a374477a5507858ac085b9d95bdf22bbd99b7
MatthiasFi <mfischer@users.noreply.github.com>
2020-04-28 13:25:13 +0200

    batch script improvements

--- submit_to_sager/Sager_counting.bat
+++ submit_to_sager/Sager_counting.bat
@@ -1,10 +1,11 @@
 @echo off
-REM set url at typical localhost and start chrome
+REM please enter the loaction of the folder containing the batch script and the folder application on your computer
+set location_submit_to_sager_folder=C:\Users\Matthias Fischer\PycharmProjects\es96\submit_to_sager
 
 REM start batch script to open up chrome after a delay
-START CALL "{path_to_submit_to_sager}\start_chrome_after_delay.bat"
+START CALL "%location_submit_to_sager_folder%\start_chrome_after_delay.bat"
 
 REM Replace the path with whatever the path to you executable is
-python "{path_to_submit_to_sager}\application\application.py %*"
+python "%location_submit_to_sager_folder%\application\application.py %*"
 
 pause
